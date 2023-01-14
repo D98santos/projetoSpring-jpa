9650ba24e1efdafe516159f10b25b396b6976c12
D98santos <ds.99102811@gmail.com>
2023-01-14 06:27:02 -0300

    Category

---
+++ src/main/java/com/projetospring/projetospring/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.projetospring.projetospring.repositories;
+
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+
+import com.projetospring.projetospring.entities.Category;
+
+public interface CategoryRepository extends JpaRepositoryImplementation<Category, Long>{
+
+}
---
+++ src/main/java/com/projetospring/projetospring/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.projetospring.projetospring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetospring.projetospring.entities.Category;
+import com.projetospring.projetospring.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> fingAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
